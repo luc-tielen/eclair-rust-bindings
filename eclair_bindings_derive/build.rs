2196ac7ab890b1ec327b67831ebf2e33458a462b
luc-tielen <luc.tielen@gmail.com>
2023-02-06 11:19:47 +0100

    Use eclair-builder crate to build and link the Eclair code

--- eclair_bindings_derive/build.rs
+++ eclair_bindings_derive/build.rs
@@ -1,9 +1,15 @@
-extern crate cc;
+extern crate eclair_builder;
+
+use std::env;
 
 fn main() {
-    // TODO: Also invoke Eclair compiler to generate the test.ll
-    cc::Build::new()
-        .compiler("clang")
-        .file("tests/fixtures/test.ll")
-        .compile("eclair");
+    // TODO remove once Eclair is self-hosted!
+    let datalog_dir = env::var("DATALOG_DIR").expect("'DATALOG_DIR' env var needs to be set!");
+
+    eclair_builder::Build::new()
+        .eclair("eclair")
+        .clang("clang-14") // Important: eclair uses LLVM14 under the hood!
+        .datalog_dir(&datalog_dir)
+        .file("tests/fixtures/test.eclair")
+        .compile();
 }
